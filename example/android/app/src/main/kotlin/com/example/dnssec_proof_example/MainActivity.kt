5b12c63393dc754df83b5976200d6a674acaa8dd
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-07-22 12:51:16 +0200

    initial commit

---
+++ example/android/app/src/main/kotlin/com/example/dnssec_proof_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dnssec_proof_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Classes/dnssec_proof.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../dnssec_proof.podspec for more information.
+#include "../../src/dnssec_proof.c"
